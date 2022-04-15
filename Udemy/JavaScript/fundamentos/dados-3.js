a1a3a5d1c579d87f4951ed8b52e3b970740e4dd3
Felipe Soto <99318405+FelipeSoto01@users.noreply.github.com>
2022-04-14 23:30:04 -0300

    atualizando

---
+++ Udemy/JavaScript/fundamentos/dados-3.js
@@ -0,0 +1,17 @@
+console.log(typeof 26)
+
+let idade = 26
+console.log(typeof idade)
+
+let salario = 4787.98
+console.log(typeof salario)
+
+let  estaChovendo = true
+let noEstaChovendo = false
+console.log(typeof estaChovendo)
+console.log(typeof noEstaChovendo)
+
+
+console.log(typeof 'Teste')
+console.log(typeof "Teste")
+console.log(typeof `Teste`)
---
+++ Udemy/JavaScript/fundamentos/desafio-troca-de-valores.js
@@ -0,0 +1,7 @@
+let a = 7
+let b = 94
+console.log(a, b)
+
+a = 94
+b = 7
+console.log(a, b)
