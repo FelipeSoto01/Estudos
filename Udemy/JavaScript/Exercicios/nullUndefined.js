1fbc4778e30df112909bbdf9a92f4df2739ab5fc
Felipe Soto <99318405+FelipeSoto01@users.noreply.github.com>
2022-04-19 00:42:57 -0300

    atualizando

---
+++ Udemy/JavaScript/Exercicios/nullUndefined.js
@@ -0,0 +1,9 @@
+let valor
+console.log(valor)
+
+valor = null 
+console.log(valor)
+// console.log(valor.toString()) //Erro!
+
+const produto = {} 
+console.log(produto.preco)
--- Udemy/JavaScript/Exercicios/numerosAlgunsCuidados.js
+++ Udemy/JavaScript/Exercicios/numerosAlgunsCuidados.js
@@ -1,5 +1,6 @@
 console.log(7 / 0)
 console.log('10' / 2)
+console.log('3' + 2)    //quando soma ganha a string
 console.log('Show' * 2)
 console.log(0.1 + 0.7)
 //console.log(10.toString())
