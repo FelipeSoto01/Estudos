f4e6bbbe52ff8122959d7325027200c07a3a313b
Felipe Soto <99318405+FelipeSoto01@users.noreply.github.com>
2022-04-22 03:41:20 -0300

    atualizando

--- Udemy/JavaScript/Exercicios/nullUndefined.js
+++ Udemy/JavaScript/Exercicios/nullUndefined.js
@@ -7,3 +7,16 @@ console.log(valor)
 
 const produto = {} 
 console.log(produto.preco)
+console.log(produto)
+
+produto.preco = 3.50
+console.log(produto)
+
+produto.preco = undefined //evite atribuir undefined
+console.log(!!produto.preco)
+//delete produto.preco
+console.log(produto)
+
+produto.preco = null //sem preco
+console.log(!!produto.preco)
+console.log(produto)
